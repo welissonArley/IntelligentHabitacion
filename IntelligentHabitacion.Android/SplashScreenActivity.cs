2bacc1f73d990f2c0306ae151b33d3f37a5255c7
welissonArley <welissonarleyvs@gmail.com>
2019-12-28 22:44:42 -0300

    splash screen Ok on Android

---
+++ IntelligentHabitacion.Android/SplashScreenActivity.cs
@@ -0,0 +1,19 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace IntelligentHabitacion.Droid
+{
+    [Activity(Theme = "@style/Theme.Splash", MainLauncher = true, ScreenOrientation = ScreenOrientation.Portrait, NoHistory = true)]
+    public class SplashScreenActivity : Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            StartActivity(typeof(MainActivity));
+        }
+
+        public override void OnBackPressed() { /* Prevent the back button from canceling the startup process */ }
+    }
+}

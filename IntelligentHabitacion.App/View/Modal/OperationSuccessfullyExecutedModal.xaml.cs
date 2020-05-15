4887e8f2d690d9ea52e14a9dd72e6f34ffb1b052
welissonarley7 <welissonarley@ioasys.com.br>
2020-05-14 22:18:03 -0300

    Adding modal to show sucess or error in one operation

---
+++ IntelligentHabitacion.App/View/Modal/OperationSuccessfullyExecutedModal.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+
+namespace IntelligentHabitacion.App.View.Modal
+{
+    [DesignTimeVisible(false)]
+    public partial class OperationSuccessfullyExecutedModal : Rg.Plugins.Popup.Pages.PopupPage
+    {
+        public OperationSuccessfullyExecutedModal(string message)
+        {
+            InitializeComponent();
+
+            LabelText.Text = message;
+        }
+    }
+}

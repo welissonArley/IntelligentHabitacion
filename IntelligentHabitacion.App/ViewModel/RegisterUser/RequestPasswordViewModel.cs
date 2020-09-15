0a0012c7297f8ea4850d00efaca4ef03b7ca525b
welissonarley7 <welissonarley@ioasys.com.br>
2020-09-14 21:24:08 -0300

    Now the user can see the password

--- IntelligentHabitacion.App/ViewModel/RegisterUser/RequestPasswordViewModel.cs
+++ IntelligentHabitacion.App/ViewModel/RegisterUser/RequestPasswordViewModel.cs
@@ -2,6 +2,7 @@
 using IntelligentHabitacion.App.Services;
 using IntelligentHabitacion.App.SetOfRules.Interface;
 using IntelligentHabitacion.App.View;
+using System.ComponentModel;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Xamarin.Forms;
@@ -15,14 +16,22 @@ namespace IntelligentHabitacion.App.ViewModel.RegisterUser
         private readonly UserPreferences _userPreferences;
 
         public ICommand OnConcludeCommand { protected set; get; }
+        public ICommand ShowHidePasswordCommand { protected set; get; }
 
         public RegisterUserModel Model { get; set; }
+
+        public bool IsPassword { get; set; }
+        public string IlustrationShowHidePassword { get; set; }
+        public int IlustrationHeightRequest { get; set; }
+        public Thickness IlustrationMargin { get; set; }
 
         public RequestPasswordViewModel(IUserRule userRule, UserPreferences userPreferences)
         {
             _userRule = userRule;
             _userPreferences = userPreferences;
+            HidePassword();
             OnConcludeCommand = new Command(async () => await OnConclude());
+            ShowHidePasswordCommand = new Command(ShowHidePassword);
         }
 
         private async Task OnConclude()
@@ -53,6 +62,39 @@ namespace IntelligentHabitacion.App.ViewModel.RegisterUser
                 HideLoading();
                 await Exception(exeption);
             }
+        }
+
+        private void ShowHidePassword()
+        {
+            if (IsPassword)
+                ShowPassword();
+            else
+                HidePassword();
+        }
+
+        private void ShowPassword()
+        {
+            IsPassword = false;
+            IlustrationShowHidePassword = "IconEye.png";
+            IlustrationHeightRequest = 14;
+            IlustrationMargin = new Thickness(0, 15, 0, 0);
+            OnPropertyChanged();
+        }
+        private void HidePassword()
+        {
+            IsPassword = true;
+            IlustrationShowHidePassword = "IconEyeHidden.png";
+            IlustrationHeightRequest = 18;
+            IlustrationMargin = new Thickness(0, 13, 0, 0);
+            OnPropertyChanged();
+        }
+
+        private void OnPropertyChanged()
+        {
+            OnPropertyChanged(new PropertyChangedEventArgs("IsPassword"));
+            OnPropertyChanged(new PropertyChangedEventArgs("IlustrationShowHidePassword"));
+            OnPropertyChanged(new PropertyChangedEventArgs("IlustrationHeightRequest"));
+            OnPropertyChanged(new PropertyChangedEventArgs("IlustrationMargin"));
         }
     }
 }

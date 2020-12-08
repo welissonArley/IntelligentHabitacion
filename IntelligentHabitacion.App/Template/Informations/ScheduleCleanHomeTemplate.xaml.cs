dc2923cce975a215f2a8fa65b470808aff1e3acd
welissonarley7 <welissonarley@ioasys.com.br>
2020-12-08 10:12:34 -0300

    View user clean schedule

---
+++ IntelligentHabitacion.App/Template/Informations/ScheduleCleanHomeTemplate.xaml.cs
@@ -0,0 +1,53 @@
+﻿using IntelligentHabitacion.App.Model;
+using System.Linq;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace IntelligentHabitacion.App.Template.Informations
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScheduleCleanHomeTemplate : ContentView
+    {
+        public ScheduleModel ScheduleUser
+        {
+            get => (ScheduleModel)GetValue(ScheduleUserProperty);
+            set => SetValue(ScheduleUserProperty, value);
+        }
+
+        public static readonly BindableProperty ScheduleUserProperty = BindableProperty.Create(
+                                                        propertyName: "ScheduleUser",
+                                                        returnType: typeof(ScheduleModel),
+                                                        declaringType: typeof(ScheduleCleanHomeTemplate),
+                                                        defaultValue: null,
+                                                        defaultBindingMode: BindingMode.TwoWay,
+                                                        propertyChanged: ScheduleUserChanged);
+
+        private static void ScheduleUserChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (newValue != null)
+            {
+                var scheduleModel = (ScheduleModel)newValue;
+                var component = ((ScheduleCleanHomeTemplate)bindable);
+                component.RoomLabel.Text = scheduleModel.Room;
+
+                var dayofWeek = System.Globalization.CultureInfo.CurrentCulture.DateTimeFormat.GetDayName(scheduleModel.Date.DayOfWeek);
+                component.DateLabel.Text = $"{dayofWeek.First().ToString().ToUpper() + dayofWeek.Substring(1)} • {scheduleModel.Date.Day} {scheduleModel.Date:MMM} {scheduleModel.Date.Year}";
+                if (scheduleModel.CanBeRate)
+                {
+                    component.ButtonToRate.IsVisible = true;
+                    component.SeeDetailsLabel.IsVisible = false;
+                }
+                else
+                {
+                    component.ButtonToRate.IsVisible = false;
+                    component.SeeDetailsLabel.IsVisible = true;
+                }
+            }
+        }
+
+        public ScheduleCleanHomeTemplate()
+        {
+            InitializeComponent();
+        }
+    }
+}

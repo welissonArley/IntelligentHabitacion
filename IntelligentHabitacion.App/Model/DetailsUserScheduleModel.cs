08e2e00ce5132ffd8189d1b04f5d0ce1dbdc2a02
welissonarley7 <welissonarley@ioasys.com.br>
2021-02-06 18:56:11 -0300

    See details of the user's tasks ok

---
+++ IntelligentHabitacion.App/Model/DetailsUserScheduleModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace IntelligentHabitacion.App.Model
+{
+    public class DetailsUserScheduleModel
+    {
+        public string Name { get; set; }
+        public DateTime Month { get; set; }
+        public string ProfileColor { get; set; }
+        public ObservableCollection<TaskForTheMonthDetailsGroup> Tasks { get; set; }
+    }
+
+    public class TaskForTheMonthDetailsGroup : ObservableCollection<TaskForTheMonthDetails>
+    {
+        public DateTime Date { get; set; }
+
+        public TaskForTheMonthDetailsGroup(DateTime date, ObservableCollection<TaskForTheMonthDetails> tasks) : base(tasks)
+        {
+            Date = date;
+        }
+    }
+
+    public class TaskForTheMonthDetails
+    {
+        public string Id { get; set; }
+        public string Room { get; set; }
+        public int RatingStars { get; set; }
+        public bool CanBeRate { get; set; }
+    }
+}
--- IntelligentHabitacion.App/ViewModel/CleanHouse/DetailsUserScheduleViewModel.cs
+++ IntelligentHabitacion.App/ViewModel/CleanHouse/DetailsUserScheduleViewModel.cs
@@ -1,6 +1,89 @@
-﻿namespace IntelligentHabitacion.App.ViewModel.CleanHouse
+﻿using IntelligentHabitacion.App.Model;
+using System;
+using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace IntelligentHabitacion.App.ViewModel.CleanHouse
 {
     public class DetailsUserScheduleViewModel : BaseViewModel
     {
+        public DetailsUserScheduleModel Model { get; set; }
+
+        public ICommand SeeDetailsRatingCommand { get; private set; }
+        public ICommand RatingFriendCommand { get; private set; }
+
+        public DetailsUserScheduleViewModel()
+        {
+            Model = new DetailsUserScheduleModel
+            {
+                Name = "Welisson Arley",
+                Month = DateTime.Today,
+                ProfileColor = "#000000",
+                Tasks = new ObservableCollection<TaskForTheMonthDetailsGroup>
+                {
+                    new TaskForTheMonthDetailsGroup(new DateTime(DateTime.Today.Year, DateTime.Today.Month, 5), new ObservableCollection<TaskForTheMonthDetails>
+                    {
+                        new TaskForTheMonthDetails
+                        {
+                            Id = "1",
+                            CanBeRate = true,
+                            RatingStars = 4,
+                            Room = "Área de Serviço"
+                        }
+                    }),
+                    new TaskForTheMonthDetailsGroup(new DateTime(DateTime.Today.Year, DateTime.Today.Month, 1), new ObservableCollection<TaskForTheMonthDetails>
+                    {
+                        new TaskForTheMonthDetails
+                        {
+                            Id = "2",
+                            CanBeRate = false,
+                            RatingStars = -1,
+                            Room = "Área de Serviço"
+                        },
+                        new TaskForTheMonthDetails
+                        {
+                            Id = "2",
+                            CanBeRate = false,
+                            RatingStars = 3,
+                            Room = "Banheiro"
+                        }
+                    })
+                }
+            };
+
+            RatingFriendCommand = new Command(async () => { await RatingFriendTask(); });
+            SeeDetailsRatingCommand = new Command(async () => { await SeeDatailsRatingTask(); });
+        }
+
+        private async Task RatingFriendTask()
+        {
+            try
+            {
+                await ShowLoading();
+                await Navigation.PushAsync<RatingCleaningViewModel>();
+                HideLoading();
+            }
+            catch (System.Exception exeption)
+            {
+                HideLoading();
+                await Exception(exeption);
+            }
+        }
+        private async Task SeeDatailsRatingTask()
+        {
+            try
+            {
+                await ShowLoading();
+                await Navigation.PushAsync<SeeDetailsRatingCleaningViewModel>();
+                HideLoading();
+            }
+            catch (System.Exception exeption)
+            {
+                HideLoading();
+                await Exception(exeption);
+            }
+        }
     }
 }

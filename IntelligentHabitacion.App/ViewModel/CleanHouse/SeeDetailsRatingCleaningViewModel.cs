a5b636bc105725faec06f58c99189ceb2233f95f
welissonarley7 <welissonarley@ioasys.com.br>
2021-02-07 13:54:42 -0300

    See all feedbacks about an task ok

--- IntelligentHabitacion.App/ViewModel/CleanHouse/SeeDetailsRatingCleaningViewModel.cs
+++ IntelligentHabitacion.App/ViewModel/CleanHouse/SeeDetailsRatingCleaningViewModel.cs
@@ -1,6 +1,50 @@
-﻿namespace IntelligentHabitacion.App.ViewModel.CleanHouse
+﻿using IntelligentHabitacion.App.Model;
+using System;
+using System.Collections.ObjectModel;
+
+namespace IntelligentHabitacion.App.ViewModel.CleanHouse
 {
     public class SeeDetailsRatingCleaningViewModel : BaseViewModel
     {
+        public ObservableCollection<RatingCleaningModel> ModelList { get; set; }
+
+        public SeeDetailsRatingCleaningViewModel()
+        {
+            ModelList = new ObservableCollection<RatingCleaningModel>
+            {
+                new RatingCleaningModel
+                {
+                    Name = "Welisson",
+                    Date = DateTime.Today,
+                    Feedback = "Eu gostei",
+                    RatingStars = 5,
+                    Room = "Escritório"
+                },
+                new RatingCleaningModel
+                {
+                    Name = "Welisson",
+                    Date = DateTime.Today,
+                    Feedback = "Ok",
+                    RatingStars = 4,
+                    Room = "Escritório"
+                },
+                new RatingCleaningModel
+                {
+                    Name = "Welisson",
+                    Date = DateTime.Today,
+                    Feedback = "Ficou bom",
+                    RatingStars = 3,
+                    Room = "Escritório"
+                },
+                new RatingCleaningModel
+                {
+                    Name = "Welisson",
+                    Date = DateTime.Today,
+                    Feedback = "Faltou as janelas",
+                    RatingStars = 2,
+                    Room = "Escritório"
+                }
+            };
+        }
     }
 }

1eadff0141b8bbbd161f01d9bff9f59632244306
welissonarley7 <welissonarley@ioasys.com.br>
2021-03-20 16:12:13 -0300

    Animate skeleton loadings

---
+++ IntelligentHabitacion.App/CustomControl/SkeletonView.cs
@@ -0,0 +1,17 @@
+﻿using Xamarin.Forms;
+
+namespace IntelligentHabitacion.App.CustomControl
+{
+    public class SkeletonView : BoxView
+    {
+        public SkeletonView()
+        {
+            var smoothAnimation = new Animation();
+
+            smoothAnimation.WithConcurrent((f) => Opacity = f, 0.3, 1, Easing.SinIn);
+            smoothAnimation.WithConcurrent((f) => Opacity = f, 1, 0.3, Easing.SinIn);
+
+            this.Animate("FadeInOut", smoothAnimation, 16, 1000, Easing.SinIn, null, () => true);
+        }
+    }
+}

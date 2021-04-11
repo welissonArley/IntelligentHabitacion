701e7e72c01e2ad3b767b6dd2e210af46b7478a1
welissonarley7 <welissonarley@ioasys.com.br>
2021-04-11 13:39:56 -0300

    Get all task rate on api ok

---
+++ IntelligentHabitacion.Api.Application/UseCases/CleaningSchedule/DetailsAllRate/IDetailsAllRateUseCase.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace IntelligentHabitacion.Api.Application.UseCases.CleaningSchedule.DetailsAllRate
+{
+    public interface IDetailsAllRateUseCase
+    {
+        Task<ResponseOutput> Execute(long completedTaskId);
+    }
+}

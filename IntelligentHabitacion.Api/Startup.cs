b7b72c241fb7589d6cc57ae5bbb51e1e9b6c4e83
welissonarley7 <welissonarley@ioasys.com.br>
2021-04-13 15:08:10 -0300

    Teste AzureDevops ok

--- IntelligentHabitacion.Api/Startup.cs
+++ IntelligentHabitacion.Api/Startup.cs
@@ -16,6 +16,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using System.Linq;
 using System.Reflection;
 
 #pragma warning disable ASP0000

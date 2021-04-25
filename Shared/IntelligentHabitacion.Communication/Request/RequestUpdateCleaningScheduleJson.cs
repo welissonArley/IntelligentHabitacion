d761f05088d1d7bb376b7572330f8a9a80226ee0
welissonarley7 <welissonarley@ioasys.com.br>
2021-04-25 19:27:59 -0300

    Unit tests to create first schudule validation

--- Shared/IntelligentHabitacion.Communication/Request/RequestUpdateCleaningScheduleJson.cs
+++ Shared/IntelligentHabitacion.Communication/Request/RequestUpdateCleaningScheduleJson.cs
@@ -4,6 +4,11 @@ namespace IntelligentHabitacion.Communication.Request
 {
     public class RequestUpdateCleaningScheduleJson
     {
+        public RequestUpdateCleaningScheduleJson()
+        {
+            Rooms = new List<string>();
+        }
+
         public string UserId { get; set; }
         public IList<string> Rooms { get; set; }
     }

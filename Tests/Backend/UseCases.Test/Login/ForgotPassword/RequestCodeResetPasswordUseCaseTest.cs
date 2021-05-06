390e016813aadd9fc0c5d906365aaacd9cfeaf90
welissonarley7 <welissonarley@ioasys.com.br>
2021-05-06 10:55:04 -0300

    Unit tests to usecases about resetpassword

---
+++ Tests/Backend/UseCases.Test/Login/ForgotPassword/RequestCodeResetPasswordUseCaseTest.cs
@@ -0,0 +1,55 @@
+﻿using FluentAssertions;
+using IntelligentHabitacion.Api.Application.UseCases.Login.ForgotPassword;
+using IntelligentHabitacion.Api.Domain.Repository;
+using IntelligentHabitacion.Api.Domain.Repository.Code;
+using IntelligentHabitacion.Api.Domain.Services;
+using System;
+using System.Threading.Tasks;
+using Useful.ToTests.Builders.Email;
+using Useful.ToTests.Builders.Repositories;
+using Xunit;
+
+namespace UseCases.Test.Login.ForgotPassword
+{
+    public class RequestCodeResetPasswordUseCaseTest
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly ISendEmail _sendEmail;
+        private readonly ICodeWriteOnlyRepository _tokenWriteOnlyRepository;
+
+        public RequestCodeResetPasswordUseCaseTest()
+        {
+            _unitOfWork = UnitOfWorkBuilder.Instance().Build();
+            _sendEmail = SendEmailBuilder.Instance().Build();
+            _tokenWriteOnlyRepository = CodeWriteOnlyRepositoryBuilder.Instance().Build();
+        }
+
+        [Fact]
+        public async Task Validade_Sucess()
+        {
+            var email = "user@email.com";
+
+            var userRepository = UserReadOnlyRepositoryBuilder.Instance().GetByEmail(email, CreateUserModel(email)).Build();
+
+            var useCase = new RequestCodeResetPasswordUseCase(userRepository, _tokenWriteOnlyRepository, _sendEmail, _unitOfWork);
+
+            Func<Task> act = async () =>
+            {
+                await useCase.Execute(email);
+            };
+
+            await act.Should().NotThrowAsync();
+        }
+
+        private IntelligentHabitacion.Api.Domain.Entity.User CreateUserModel(string email)
+        {
+            return new IntelligentHabitacion.Api.Domain.Entity.User
+            {
+                Id = 1,
+                Active = true,
+                CreateDate = DateTime.UtcNow,
+                Email = email
+            };
+        }
+    }
+}
---
+++ Tests/Useful.ToTests/Builders/Repositories/UserUpdateOnlyRepositoryBuilder.cs
@@ -0,0 +1,37 @@
+﻿using IntelligentHabitacion.Api.Domain.Entity;
+using IntelligentHabitacion.Api.Domain.Repository.User;
+using Moq;
+
+namespace Useful.ToTests.Builders.Repositories
+{
+    public class UserUpdateOnlyRepositoryBuilder
+    {
+        private static UserUpdateOnlyRepositoryBuilder _instance;
+        private readonly Mock<IUserUpdateOnlyRepository> _repository;
+
+        private UserUpdateOnlyRepositoryBuilder()
+        {
+            if (_repository == null)
+            {
+                _repository = new Mock<IUserUpdateOnlyRepository>();
+            }
+        }
+
+        public static UserUpdateOnlyRepositoryBuilder Instance()
+        {
+            _instance = new UserUpdateOnlyRepositoryBuilder();
+            return _instance;
+        }
+
+        public UserUpdateOnlyRepositoryBuilder GetByEmail(string email, User user)
+        {
+            _repository.Setup(x => x.GetByEmail_Update(email)).ReturnsAsync(user);
+            return this;
+        }
+
+        public IUserUpdateOnlyRepository Build()
+        {
+            return _repository.Object;
+        }
+    }
+}

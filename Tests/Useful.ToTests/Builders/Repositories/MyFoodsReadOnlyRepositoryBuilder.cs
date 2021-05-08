d0d9115569def49281a3fedd5f3e60b533a27b1a
welissonarley7 <welissonarley@ioasys.com.br>
2021-05-08 08:37:46 -0300

    Unit test to useCase ChangeQuantityOfOneProduct

---
+++ Tests/Useful.ToTests/Builders/Repositories/MyFoodsReadOnlyRepositoryBuilder.cs
@@ -0,0 +1,37 @@
+﻿using IntelligentHabitacion.Api.Domain.Entity;
+using IntelligentHabitacion.Api.Domain.Repository.MyFoods;
+using Moq;
+
+namespace Useful.ToTests.Builders.Repositories
+{
+    public class MyFoodsReadOnlyRepositoryBuilder
+    {
+        private static MyFoodsReadOnlyRepositoryBuilder _instance;
+        private readonly Mock<IMyFoodsReadOnlyRepository> _repository;
+
+        private MyFoodsReadOnlyRepositoryBuilder()
+        {
+            if (_repository == null)
+            {
+                _repository = new Mock<IMyFoodsReadOnlyRepository>();
+            }
+        }
+
+        public static MyFoodsReadOnlyRepositoryBuilder Instance()
+        {
+            _instance = new MyFoodsReadOnlyRepositoryBuilder();
+            return _instance;
+        }
+
+        public MyFoodsReadOnlyRepositoryBuilder GetById(long userId, MyFood myFood)
+        {
+            _repository.Setup(x => x.GetById(myFood.Id, userId)).ReturnsAsync(myFood);
+            return this;
+        }
+
+        public IMyFoodsReadOnlyRepository Build()
+        {
+            return _repository.Object;
+        }
+    }
+}

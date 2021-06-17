786f95895f4141c26edc06d474c8b4a23a2264b7
welissonarley7 <welissonarley@ioasys.com.br>
2021-06-17 17:07:05 -0300

    Adding Entities and enums on new project domain

---
+++ src/Backend/Homuai.Domain/Entity/User.cs
@@ -0,0 +1,26 @@
+﻿using Homuai.Domain.ValueObjects;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Homuai.Domain.Entity
+{
+    [Table("User")]
+    public class User : EntityBase
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string ProfileColor { get; set; }
+        public string PushNotificationId { get; set; }
+        public ICollection<Phonenumber> Phonenumbers { get; set; }
+        public ICollection<EmergencyContact> EmergencyContacts { get; set; }
+
+        public long? HomeAssociationId { get; set; }
+        public HomeAssociation HomeAssociation { get; set; }
+
+        public bool IsAdministrator()
+        {
+            return HomeAssociation != null && HomeAssociation.Home.AdministratorId == Id;
+        }
+    }
+}

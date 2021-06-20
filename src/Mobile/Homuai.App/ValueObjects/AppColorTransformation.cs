fe7703cfe9955ee93f765520942c3b51d04bba16
welissonarley7 <welissonarley@ioasys.com.br>
2021-06-20 09:54:42 -0300

    Adding value objects on new app project

---
+++ src/Mobile/Homuai.App/ValueObjects/AppColorTransformation.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace Homuai.App.ValueObjects
+{
+    public class AppColorTransformation : FFImageLoading.Transformations.TintTransformation
+    {
+        public AppColorTransformation()
+        {
+            HexColor = Application.Current.RequestedTheme == OSAppTheme.Light ? "#000000" : "#FFFFFF";
+            EnableSolidColor = true;
+        }
+    }
+}

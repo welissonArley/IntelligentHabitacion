066522daed3665cd65961e99f88b51d7cca5d4e5
welissonarley7 <welissonarley@ioasys.com.br>
2021-06-20 16:15:39 -0300

    Usecases about home ok

---
+++ src/Mobile/Homuai.App/UseCases/Home/Strategy/ContextStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Homuai.App.Model;
+
+namespace Homuai.App.UseCases.Home.Strategy
+{
+    public class ContextStrategy
+    {
+        public HomeRegisterStrategy GetStrategy(CountryModel country)
+        {
+            switch (country.Id)
+            {
+                case Useful.CountryEnum.BRAZIL:
+                    {
+                        return new BrazilHomeRegisterStrategy();
+                    }
+                default:
+                    {
+                        return new OthersHomeRegisterStrategy();
+                    }
+            }
+        }
+    }
+}

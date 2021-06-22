33c1a5bf5195129bfd78ef37af7b16a9bb7e0681
welissonarley7 <welissonarley@ioasys.com.br>
2021-06-22 15:56:51 -0300

    Getstarted view model

---
+++ src/Mobile/Homuai.App/ViewModel/User/Register/RequestEmailViewModel.cs
@@ -0,0 +1,9 @@
+﻿using Homuai.App.Model;
+
+namespace Homuai.App.ViewModel.User.Register
+{
+    public class RequestEmailViewModel : BaseViewModel
+    {
+        public RegisterUserModel Model { get; set; }
+    }
+}

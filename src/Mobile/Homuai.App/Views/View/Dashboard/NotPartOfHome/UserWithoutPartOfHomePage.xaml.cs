da9d1c98600324e79fff1811af9fa352f2ed8ec3
welissonarley7 <welissonarley@ioasys.com.br>
2021-06-22 14:11:44 -0300

    dashboard views on the new app project

---
+++ src/Mobile/Homuai.App/Views/View/Dashboard/NotPartOfHome/UserWithoutPartOfHomePage.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XLabs.Forms.Mvvm;
+
+namespace Homuai.App.Views.View.Dashboard.NotPartOfHome
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class UserWithoutPartOfHomePage : FlyoutPage
+    {
+        public UserWithoutPartOfHomePage()
+        {
+            InitializeComponent();
+            /*
+            Flyout = (Page)ViewFactory.CreatePage<UserWithoutPartOfHomeFlyoutViewModel, UserWithoutPartOfHomeFlyoutViewModel>();
+
+            Detail.BindingContext = new UserWithoutPartOfHomeDetailViewModel(Navigation);*/
+        }
+    }
+}
